99df8e358e9e0434f62c59699a378feef130171c
anantpatan <84713024+anantpatan@users.noreply.github.com>
2021-05-30 23:16:28 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -30,3 +30,42 @@ function send(){
         window.alert("Enter 2 numbers to send.");
     }
 }
+Q_turn="player1";
+A_turn="player2";
+
+function check(){
+    player_answer=document.getElementById("input_check_box").value;
+    console.log(player_answer);
+    
+    if(actual_answer==player_answer){
+        window.alert("Correct! Good.");
+        if(A_turn=="player1"){
+            score1=score1+1;
+            document.getElementById("score_1").innerHTML=score1;
+        }
+        else{
+            score2++;
+            document.getElementById("score_2").innerHTML=score2;
+        }
+    }
+    else{
+        window.alert("Oops wrong! "+actual_answer+" was the correct answer.");
+    }
+    if(Q_turn=="player1"){
+        Q_turn="player2";
+        document.getElementById("question").innerHTML="Question turn- "+player2_name;
+    }
+    else{
+        Q_turn="player1";
+        document.getElementById("question").innerHTML="Question turn- "+player1_name;
+    }
+    if(A_turn=="player1"){
+        A_turn="player2";
+        document.getElementById("answer").innerHTML="Answer turn- "+player2_name;
+    }
+    else{
+        A_turn="player1";
+        document.getElementById("answer").innerHTML="Answer turn- "+player1_name;
+    }
+    document.getElementById("output").innerHTML="";
+}
